d97231d1e8e75f21689167eeca1de05c1d4670d8
Arshhh07-ux <priyankamishra010709@gmail.com>
2026-09-11 21:32:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -898,3 +898,16 @@ console.log(
     "%cPremium Ganesh Chaturthi Website Loaded",
     "font-size:14px;"
 );
+/* =====================================================
+   26. CONSOLE
+===================================================== */
+
+console.log(
+    "%c🙏 GANPATI BAPPA MORIYA 🙏",
+    "font-size:20px;font-weight:bold;"
+);
+
+console.log(
+    "%cPremium Ganesh Chaturthi Website Loaded",
+    "font-size:14px;"
+);
